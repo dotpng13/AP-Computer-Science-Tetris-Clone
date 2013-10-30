fcae08512b669c5a363dea80d529615c974fc9c4
firecopy <firecopyx@gmail.com>
2013-10-30 11:14:49 -0500

    First Commit On Mac!

--- TetrisProject/src/Test.java
+++ TetrisProject/src/Test.java
@@ -1,7 +1,11 @@
 
 public class Test {
-//Put your name down as a comment, if you have sucessfully cloned the project
-//Andy
+//Put your name down as a comment,
+//if you have sucessfully cloned the project.
+//Don't forget to try to commit and push the project.
+//To your Fork
+
+//Andy (Windows and Mac)
 //
 //
 }
